b008d357d9b9003cdf42f315557d94f44b7877f0
PowVT <epowlowsky@gmail.com>
2021-10-27 11:44:23 -0400

    quick react native tutorial

--- App.js
+++ App.js
@@ -1,21 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { 
+  StyleSheet,
+  Text, 
+  View, 
+  TouchableWithoutFeedback, 
+  TouchableOpacity, 
+  Image, 
+  SafeAreaView, 
+  Button, 
+  Alert,
+  Platform,
+  StatusBar
+} from 'react-native';
+import { useDimensions } from '@react-native-community/hooks';
 
 export default function App() {
+
+  console.log(useDimensions)
+
+  const handlePress = () => {console.log("Handle Press")}
+  
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <Text numberOfLines={1} onPress={handlePress}>
+        Hello Evan 
+      </Text>
+      <TouchableOpacity >
+        <Image source={require("./assets/10_22_20218_40_23AM.jpg")} style={{ width: 250, height: 350 }} fadeDuration={750}/>
+      </TouchableOpacity>
+        <Button 
+        title="Alert"
+        onPress={() =>{Alert.alert("Hungry Squid Game Salem", "This ferocious beast likes to feast on hooman food with her partner in crime Mr. Seymour.", [
+          {text: "Yes", onPress: () => console.log("Yes pressed")},
+          {text: "No"}
+        ])}}/>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'lightblue',
+    paddingTop:Platform.OS === "android" ? StatusBar.currentHeight: 0,
     alignItems: 'center',
-    justifyContent: 'center',
   },
 });
---
+++ README.md
@@ -0,0 +1,45 @@
+# First React Native Project
+
+## Quick Start
+
+Open Android Studio and select the AVD (Android Virtual device) you want to use. This can be found under 'More Actions' in the start screen. Then select AVD Manager. Next, choose from a virtual device you have already created or 'Create Virtual Device'. Once the AVD is active you can run the command `bash expo start` in the root of the directory. Then press `bash a ` to open the android emulator or select it from the 'Expo Developer tools' window which launches on localhost when starting.
+
+## Notes
+
+views = divs
+(SafeAreaView is better than View, especially on apples products.)
+
+local images use:
+
+```bash
+<Image source={require("./assets/splash.png")}/>
+```
+
+external images use:
+
+```bash
+<Image
+    source={{
+        width:200, // also can use percentages
+        height300,
+        uri:"some-url-here"
+    }}
+/>
+```
+
+'Buttons' are a subset of 'touchables'.
+
+Default Styling:
+
+```bash
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'lightblue',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+```
+
+For both portrait and landscape orientations, chnage app.json property 'orientation' to 'default' instead of 'portrait' or 'landscape'.
